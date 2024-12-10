afe0978c76db733284e95cc358dd7957432e5bc4
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-12-10 10:24:55 -0300

    feat: add Loading, Steps, Step and Welcome components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
 
+import {
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+  useFonts,
+} from "@expo-google-fonts/rubik";
+import { Loading } from "@/components/loading";
+
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <Stack
       screenOptions={{
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
+import { Steps } from "@/components/steps";
+import { Welcome } from "@/components/welcome";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 22 }}>Hello React-Native!</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+
+      <Steps />
     </View>
   );
 }
